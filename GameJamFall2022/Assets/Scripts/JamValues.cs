e09d0a2587b89f285bc26c2dd07b3c07cec758fb
zeyogh <zeynep.ogh@gmail.com>
2022-11-11 23:44:37 -0600

    Added more code

--- GameJamFall2022/Assets/Scripts/JamValues.cs
+++ GameJamFall2022/Assets/Scripts/JamValues.cs
@@ -7,16 +7,38 @@ public class JamValues : MonoBehaviour
     /*
      * [Fruit, Cut, Sugar, Mix]
      */
-    private int[] jamValues;
+    private static int[] jamValues;
 
     void Awake()
     {
-        jamValues = new int[4];
+        jamValues = new int[3] { -1, -1, -1};
     }
 
+    /*
+     * 0 = apple, 1 = strawberry, 2 = blueberry, 3 = mystery meat
+     */
     public void updateFruit(int fruit)
     {
-        int[0] = fruit;
+        jamValues[0] = fruit;
+        Debug.Log(jamValues.ToString() + jamValues[0]);
+    }
+
+    /*
+     * 0 = whole, 2 = cut, 4 = crushed
+     */
+    public void updateCut(int cut)
+    {
+        jamValues[1] = cut;
+    }
+
+    public void updateSugar(int sugar)
+    {
+        jamValues[2] = sugar;
+    }
+
+    public void cut()
+    {
+        jamValues[1]++;
     }
 
 }
