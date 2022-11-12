a56e6e1d27ca043a65024f42f133601390276f9e
eldritchalpaca <118037673+eldritchalpaca@users.noreply.github.com>
2022-11-12 16:19:28 -0600

    all dialogue inputted

--- GameJamFall2022/Assets/Scripts/DIALOGUE ZONE/DialogueScripts/DialogueTrigger.cs
+++ GameJamFall2022/Assets/Scripts/DIALOGUE ZONE/DialogueScripts/DialogueTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogueTrigger : MonoBehaviour
 {
@@ -27,7 +28,7 @@ public class DialogueTrigger : MonoBehaviour
 
         if (!jamMatches && character.name == "Coyote")
         {
-            //      FindObjectOfType<SceneChanger>().changeScenes()
+            SceneManager.LoadScene("CoyoteGameOver");
         }
     }
 }
